a513dd592d4b02508969a015628fdbf86ed2356d
Fan <136318963+0heiheihei0@users.noreply.github.com>
2024-03-16 23:59:07 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,6 @@
-﻿document.querySelector("p").addEventListener("click", function() {
+document.querySelector("li").addEventListener("click", function() {
   alert("别学啦，我害怕！");
 });
-
-let myImage = document.querySelector("img");
-
-myImage.onclick = function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/头像.jpg") {
-    myImage.setAttribute("src", "images/头像.jpg");
-  } else {
-    myImage.setAttribute("src", "images/头像.jpg");
-  }
-};
 
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
